96d3ac7af426cfa7fa01739118c79ea756b94abd
Martin Vysny <mavi@vaadin.com>
2023-05-23 14:14:18 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,103 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    `maven-publish`
+    signing
+}
+
+allprojects {
+    group = "com.github.mvysny.shepherd"
+    version = "0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+defaultTasks("clean", "build")
+
+subprojects {
+    apply {
+        plugin("maven-publish")
+        plugin("kotlin")
+        plugin("org.gradle.signing")
+    }
+    
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    // creates a reusable function which configures proper deployment to Bintray
+    ext["configureBintray"] = { artifactId: String ->
+
+        java {
+            withJavadocJar()
+            withSourcesJar()
+        }
+
+        tasks.withType<Javadoc> {
+            isFailOnError = false
+        }
+
+        publishing {
+            repositories {
+                maven {
+                    setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                        password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+                    }
+                }
+            }
+            publications {
+                create("mavenJava", MavenPublication::class.java).apply {
+                    groupId = project.group.toString()
+                    this.artifactId = artifactId
+                    version = project.version.toString()
+                    pom {
+                        description.set("Vaadin Shepherd Java Client")
+                        name.set(artifactId)
+                        url.set("https://github.com/mvysny/shepherd-java-client")
+                        licenses {
+                            license {
+                                name.set("The Apache Software License, Version 2.0")
+                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                distribution.set("repo")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("mavi")
+                                name.set("Martin Vysny")
+                                email.set("martin@vysny.me")
+                            }
+                        }
+                        scm {
+                            url.set("https://github.com/mvysny/shepherd-java-client")
+                        }
+                    }
+                    from(components["java"])
+                }
+            }
+        }
+
+        signing {
+            sign(publishing.publications["mavenJava"])
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            // to see the exceptions of failed tests in Travis-CI console.
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+include("shepherd-cli", "shepherd-java-api")
