1188f34083c41b46304f9a900a48db546c5df82c
Martin Vysny <mavi@vaadin.com>
2023-05-25 15:33:56 +0300

    SimpleKubernetesClient now runs the correct binary

--- shepherd-java-api/src/main/kotlin/SimpleKubernetesClient.kt
+++ shepherd-java-api/src/main/kotlin/SimpleKubernetesClient.kt
@@ -4,12 +4,12 @@ package com.github.mvysny.shepherd.api
  * A very simple Kubernetes client, retrieves stuff by running the kubectl binary.
  * @property kubectl the kubectl binary
  */
-public class SimpleKubernetesClient(private val kubectl: String = "mkctl") {
+public class SimpleKubernetesClient(private val kubectl: Array<String> = arrayOf("microk8s", "kubectl")) {
     /**
      * Runs `kubectl get pods` and returns all names.
      */
     public fun getPods(namespace: String): List<String> {
-        val stdout = exec(kubectl, "get", "pods", "--namespace", namespace)
+        val stdout = exec(*kubectl, "get", "pods", "--namespace", namespace)
         return stdout.lines()
             .drop(1)
             .map { it.split(' ').first() }
@@ -19,5 +19,5 @@ public class SimpleKubernetesClient(private val kubectl: String = "mkctl") {
      * Returns the logs of given pod.
      */
     public fun getLogs(podName: String, namespace: String): String =
-        exec(kubectl, "logs", podName, "--namespace", namespace)
+        exec(*kubectl, "logs", podName, "--namespace", namespace)
 }
