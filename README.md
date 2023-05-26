bd165e09d1794f64685ec26be810f8d210262ba5
Martin Vysny <mavi@vaadin.com>
2023-05-26 18:23:43 +0300

    readme: add json example

--- README.md
+++ README.md
@@ -24,3 +24,82 @@ the `shepherd-cli/build/distributions/*.zip` to the target machine which runs Sh
 then unzip and run the `shepherd-cli` binary.
 
 Shepherd CLI requires Java 17+.
+
+`shepherd-cli create` requires the project descriptor json. It's really simple,
+here's a very simple example for the [vaadin-boot-example-gradle](https://github.com/mvysny/vaadin-boot-example-gradle) project:
+
+```json
+{
+  "id": "vaadin-boot-example-gradle",
+  "description": "vaadin-boot-example-gradle",
+  "gitRepo": {
+    "url": "https://github.com/mvysny/vaadin-boot-example-gradle",
+    "branch": "master"
+  },
+  "owner": {
+    "name": "Martin Vysny",
+    "email": "mavi@vaadin.com"
+  },
+  "runtime": {
+    "resources": {
+      "memoryMb": 256,
+      "cpu": 1.0
+    }
+  },
+  "build": {
+    "resources": {
+      "memoryMb": 2048,
+      "cpu": 2.0
+    }
+  }
+}
+```
+
+A more complex example:
+
+```json
+{
+  "id": "jdbi-orm-vaadin-crud-demo",
+  "description": "JDBI-ORM example project",
+  "gitRepo": {
+    "url": "https://github.com/mvysny/jdbi-orm-vaadin-crud-demo",
+    "branch": "master"
+  },
+  "owner": {
+    "name": "Martin Vysny",
+    "email": "mavi@vaadin.com"
+  },
+  "runtime": {
+    "resources": {
+      "memoryMb": 256,
+      "cpu": 1.0
+    },
+    "envVars": {
+      "JDBC_URL": "jdbc:postgresql://postgres-service:5432/postgres",
+      "JDBC_USERNAME": "postgres",
+      "JDBC_PASSWORD": "mysecretpassword"
+    }
+  },
+  "build": {
+    "resources": {
+      "memoryMb": 2048,
+      "cpu": 2.0
+    },
+    "buildArgs": {
+      "offlinekey": "q3984askdjalkd9823"
+    },
+    "dockerFile": "vherd.Dockerfile"
+  },
+  "publication": {
+    "publishOnMainDomain": false,
+    "additionalDomains": [
+      "demo.jdbiorm.eu"
+    ]
+  },
+  "additionalServices": [
+    {
+      "type": "Postgres"
+    }
+  ]
+}
+```
--- shepherd-java-api/src/test/kotlin/ProjectTest.kt
+++ shepherd-java-api/src/test/kotlin/ProjectTest.kt
@@ -6,13 +6,42 @@ import kotlin.test.expect
 
 val fakeProject = Project(
     id = ProjectId("vaadin-boot-example-gradle"),
-    description = "vaadin-boot-example-gradle",
+    description = "Example project for Vaadin Boot built via Gradle",
     gitRepo = GitRepo("https://github.com/mvysny/vaadin-boot-example-gradle", "master"),
     owner = ProjectOwner("Martin Vysny", "mavi@vaadin.com"),
     runtime = ProjectRuntime(Resources.defaultRuntimeResources),
     build = Build(resources = Resources.defaultBuildResources)
 )
-private val serializedJson = """{"id":"vaadin-boot-example-gradle","description":"vaadin-boot-example-gradle","gitRepo":{"url":"https://github.com/mvysny/vaadin-boot-example-gradle","branch":"master"},"owner":{"name":"Martin Vysny","email":"mavi@vaadin.com"},"runtime":{"resources":{"memoryMb":256,"cpu":1.0}},"build":{"resources":{"memoryMb":2048,"cpu":2.0}}}"""
+private val serializedJson = """{"id":"vaadin-boot-example-gradle","description":"Example project for Vaadin Boot built via Gradle","gitRepo":{"url":"https://github.com/mvysny/vaadin-boot-example-gradle","branch":"master"},"owner":{"name":"Martin Vysny","email":"mavi@vaadin.com"},"runtime":{"resources":{"memoryMb":256,"cpu":1.0}},"build":{"resources":{"memoryMb":2048,"cpu":2.0}}}"""
+
+val fakeProject2 = Project(
+    id = ProjectId("jdbi-orm-vaadin-crud-demo"),
+    description = "JDBI-ORM example project",
+    gitRepo = GitRepo("https://github.com/mvysny/jdbi-orm-vaadin-crud-demo", "master"),
+    owner = ProjectOwner("Martin Vysny", "mavi@vaadin.com"),
+    runtime = ProjectRuntime(
+        Resources.defaultRuntimeResources,
+        envVars = mapOf(
+            "JDBC_URL" to "jdbc:postgresql://postgres-service:5432/postgres",
+            "JDBC_USERNAME" to "postgres",
+            "JDBC_PASSWORD" to "mysecretpassword"
+        )
+    ),
+    build = Build(
+        resources = Resources.defaultBuildResources,
+        buildArgs = mapOf(
+            "offlinekey" to "q3984askdjalkd9823"
+        ),
+        dockerFile = "vherd.Dockerfile"
+    ),
+    publication = Publication(
+        publishOnMainDomain = false,
+        additionalDomains = setOf("demo.jdbiorm.eu")
+    ),
+    additionalServices = setOf(Service(ServiceType.Postgres))
+)
+private val serializedJson2 = """{"id":"jdbi-orm-vaadin-crud-demo","description":"JDBI-ORM example project","gitRepo":{"url":"https://github.com/mvysny/jdbi-orm-vaadin-crud-demo","branch":"master"},"owner":{"name":"Martin Vysny","email":"mavi@vaadin.com"},"runtime":{"resources":{"memoryMb":256,"cpu":1.0},"envVars":{"JDBC_URL":"jdbc:postgresql://postgres-service:5432/postgres","JDBC_USERNAME":"postgres","JDBC_PASSWORD":"mysecretpassword"}},"build":{"resources":{"memoryMb":2048,"cpu":2.0},"buildArgs":{"offlinekey":"q3984askdjalkd9823"},"dockerFile":"vherd.Dockerfile"},"publication":{"publishOnMainDomain":false,"additionalDomains":["demo.jdbiorm.eu"]},"additionalServices":[{"type":"Postgres"}]}"""
+
 
 class ProjectIdTest : DynaTest({
     test("validation pass") {
@@ -38,8 +67,10 @@ class ProjectIdTest : DynaTest({
 class ProjectTest : DynaTest({
     test("json serialization") {
         expect(serializedJson) { fakeProject.toJson() }
+        expect(serializedJson2) { fakeProject2.toJson() }
     }
     test("json deserialization") {
         expect(fakeProject) { Project.fromJson(serializedJson) }
+        expect(fakeProject2) { Project.fromJson(serializedJson2) }
     }
 })
