31fbfcea6add3752722ca00e51a10f59c70aa016
Martin Vysny <mavi@vaadin.com>
2023-06-01 13:30:50 +0300

    fix tests

--- shepherd-java-api/src/test/kotlin/ShepherdClientTest.kt
+++ shepherd-java-api/src/test/kotlin/ShepherdClientTest.kt
@@ -6,22 +6,22 @@ import kotlin.test.expect
 class ShepherdClientTest : DynaTest({
     group("Stats") {
         test("empty") {
-            expect(Stats(2, 2, 0, 0)) {
+            expect(Stats(2, 2, 0, 0, 0)) {
                 Stats.calculate(Config(2, 2), listOf())
             }
         }
         test("1 project") {
-            expect(Stats(2, 2, 256, 2304)) {
+            expect(Stats(2, 2, 256, 2304, 1)) {
                 Stats.calculate(Config(2, 2), listOf(fakeProject))
             }
         }
         test("2 projects") {
-            expect(Stats(2, 2, 512, 4608)) {
+            expect(Stats(2, 2, 512, 4608, 2)) {
                 Stats.calculate(Config(2, 2), listOf(fakeProject, fakeProject2))
             }
         }
         test("2 projects 1 builder") {
-            expect(Stats(2,1,  512, 2560)) {
+            expect(Stats(2,1,  512, 2560, 2)) {
                 Stats.calculate(Config(2, 1), listOf(fakeProject, fakeProject2))
             }
         }
