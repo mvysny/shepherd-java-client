9aa715998760b2cb55c9337dcb3e3ffea935b749
Martin Vysny <mavi@vaadin.com>
2023-06-01 12:03:22 +0300

    #8 Implement Config.kt which loads shepherd config file

---
+++ shepherd-java-api/src/main/kotlin/Config.kt
@@ -0,0 +1,37 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
+package com.github.mvysny.shepherd.api
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.inputStream
+
+/**
+ * @property maxAvailableMemoryMb The max memory for Shepherd Kubernetes to run VM stuff - projects and their builds.
+ * That's the machine memory minus Jenkins usage (by default 512mb) minus Kubernetes itself (say 512mb),
+ * possibly minus 256mb for the future shepherd-ui project
+ * @property concurrentJenkinsBuilders Number of concurrent job runners in Jenkins. Defaults to 2,
+ * can be seen as `# of executors` at `http://localhost:8080/manage/configure`.
+ *
+ */
+@Serializable
+public data class Config(
+    val maxAvailableMemoryMb: Int,
+    val concurrentJenkinsBuilders: Int,
+) {
+    public companion object {
+        /**
+         * Location of the config file on the filesystem.
+         */
+        public val location: Path = Path("/etc/shepherd/java/config.json")
+
+        /**
+         * Loads the current config file from the filesystem (from [location]).
+         */
+        public fun load(): Config = location.inputStream().buffered().use { Json.decodeFromStream(it) }
+    }
+}
