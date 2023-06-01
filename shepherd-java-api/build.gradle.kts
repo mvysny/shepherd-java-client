370dac8aa0d0dc875121a2ae2d37fd731d6454ec
Martin Vysny <mavi@vaadin.com>
2023-06-01 19:40:21 +0300

    Remove unused dep

--- shepherd-java-api/build.gradle.kts
+++ shepherd-java-api/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.zeroturnaround:zt-exec:1.12")
-//    implementation("com.offbytwo.jenkins:jenkins-client:0.3.8")
     implementation("org.slf4j:slf4j-api:2.0.6")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
     implementation("com.squareup.okhttp3:okhttp:4.10.0") // communicates with Jenkins
