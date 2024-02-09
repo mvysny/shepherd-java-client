70b27ffe248bafc1b5503e67bd3f3ad164624dc4
Martin Vysny <mavi@vaadin.com>
2024-02-09 10:34:34 +0200

    dep v bump

--- shepherd-java-api/build.gradle.kts
+++ shepherd-java-api/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.zeroturnaround:zt-exec:1.12")
-    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("org.slf4j:slf4j-api:2.0.11")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
 }
 
