d42656598f08cbba1f903a50427ab36f1a6ade42
Martin Vysny <mavi@vaadin.com>
2024-07-02 11:09:39 +0300

    gradle warning fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -90,7 +91,7 @@ subprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        compilerOptions.jvmTarget = JvmTarget.JVM_17
     }
 
     tasks.withType<Test> {
