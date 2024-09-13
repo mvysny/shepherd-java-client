43ae97a41cc79bfb594392c52f010b762c18ad62
Martin Vysny <mavi@vaadin.com>
2024-09-14 01:04:50 +0300

    MainLayout: use sidenav

--- shepherd-web/src/main/kotlin/ui/MainLayout.kt
+++ shepherd-web/src/main/kotlin/ui/MainLayout.kt
@@ -2,16 +2,18 @@ package com.github.mvysny.shepherd.web.ui
 
 import com.github.mvysny.karibudsl.v10.button
 import com.github.mvysny.karibudsl.v10.drawer
+import com.github.mvysny.karibudsl.v10.drawerToggle
 import com.github.mvysny.karibudsl.v10.h3
 import com.github.mvysny.karibudsl.v10.h4
 import com.github.mvysny.karibudsl.v10.navbar
 import com.github.mvysny.karibudsl.v10.onClick
-import com.github.mvysny.karibudsl.v10.routerLink
 import com.github.mvysny.karibudsl.v10.span
-import com.github.mvysny.karibudsl.v10.verticalLayout
+import com.github.mvysny.karibudsl.v23.route
+import com.github.mvysny.karibudsl.v23.sideNav
 import com.github.mvysny.shepherd.web.security.UserLoginService
 import com.github.mvysny.shepherd.web.security.getCurrentUser
 import com.vaadin.flow.component.applayout.AppLayout
+import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.html.H4
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.router.PageTitle
@@ -20,20 +22,22 @@ class MainLayout : AppLayout() {
     private lateinit var routeTitle: H4
     init {
         navbar {
+            drawerToggle()
             h3("Vaadin Shepherd")
             span("/")
             routeTitle = h4("")
         }
         drawer {
-            verticalLayout(padding = false) {
-                routerLink(VaadinIcon.LIST, "Project List", ProjectListRoute::class)
+            sideNav {
+                route(ProjectListRoute::class, VaadinIcon.LIST, "Project List")
                 if (getCurrentUser().isAdmin) {
-                    routerLink(VaadinIcon.USER, "Users", UsersRoute::class)
+                    route(UsersRoute::class, VaadinIcon.USER, "Users")
                 }
-                button("Log out") {
-                    onClick {
-                        UserLoginService.get().logout()
-                    }
+            }
+            button("Log out") {
+                addThemeVariants(ButtonVariant.LUMO_SMALL)
+                onClick {
+                    UserLoginService.get().logout()
                 }
             }
         }
