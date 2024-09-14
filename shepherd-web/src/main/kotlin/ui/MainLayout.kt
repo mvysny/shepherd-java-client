ba44f1279387216b20410dcd7468f9e264da3c37
Martin Vysny <mavi@vaadin.com>
2024-09-14 10:07:33 +0300

    minor

--- shepherd-web/src/main/kotlin/ui/MainLayout.kt
+++ shepherd-web/src/main/kotlin/ui/MainLayout.kt
@@ -1,24 +1,19 @@
 package com.github.mvysny.shepherd.web.ui
 
-import com.github.mvysny.karibudsl.v10.button
 import com.github.mvysny.karibudsl.v10.drawer
 import com.github.mvysny.karibudsl.v10.drawerToggle
 import com.github.mvysny.karibudsl.v10.h3
-import com.github.mvysny.karibudsl.v10.h4
 import com.github.mvysny.karibudsl.v10.isExpand
 import com.github.mvysny.karibudsl.v10.item
 import com.github.mvysny.karibudsl.v10.menuBar
 import com.github.mvysny.karibudsl.v10.navbar
-import com.github.mvysny.karibudsl.v10.onClick
 import com.github.mvysny.karibudsl.v10.span
 import com.github.mvysny.karibudsl.v23.route
 import com.github.mvysny.karibudsl.v23.sideNav
 import com.github.mvysny.shepherd.web.security.UserLoginService
 import com.github.mvysny.shepherd.web.security.getCurrentUser
 import com.vaadin.flow.component.applayout.AppLayout
-import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.html.H3
-import com.vaadin.flow.component.html.H4
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.router.PageTitle
 
