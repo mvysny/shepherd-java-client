ab0df3bafa8433facc28ed31c7aae39875a78bd7
Martin Vysny <mavi@vaadin.com>
2024-09-27 12:26:18 +0300

    minor

--- shepherd-web/src/main/kotlin/ui/EditProjectRoute.kt
+++ shepherd-web/src/main/kotlin/ui/EditProjectRoute.kt
@@ -119,7 +119,7 @@ class ProjectForm(val creatingNew: Boolean) : KFormLayout(), Form<MutableProject
         h3("Runtime") {
             colspan = 2
         }
-        integerField("How much memory the project needs for running, in MB. Please try to keep the memory requirements as low as possible, so that we can host as many projects as possible. 256MB is a good default, but Spring Boot project may require 350MB or more.") {
+        integerField("How much memory the project needs for running, in MB. Please try to keep the memory requirements as low as possible, so that we can host as many projects as possible. 256MB is a good default, but Spring Boot project may require 350MB or more. WARNING: This is a HARD limit for the app. If JVM asks for more, it will be hard-killed by the Linux OOM-killer, without any warning or any log message (only host OS dmesg will log this). Make sure to use -Xmx256m (or a bit lower value than that, to give a bit of room for JVM itself) - then the app will crash with OutOfMemoryException which should be visible in the logs.") {
             bind(binder)
                 .withValidator(SerializablePredicate { it == null || it <= config.maxProjectRuntimeResources.memoryMb }, "Can not be larger than ${config.maxProjectRuntimeResources.memoryMb}")
                 .bind(MutableProject::runtimeMemoryMb)
