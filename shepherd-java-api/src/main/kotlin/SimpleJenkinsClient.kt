5ca35aee6f4e09d161668a6e7091524db23caa0b
Martin Vysny <mavi@vaadin.com>
2024-12-18 15:54:22 +0200

    minor

--- shepherd-java-api/src/main/kotlin/SimpleJenkinsClient.kt
+++ shepherd-java-api/src/main/kotlin/SimpleJenkinsClient.kt
@@ -247,7 +247,8 @@ internal class SimpleJenkinsClient @JvmOverloads constructor(
         fun needsProjectRebuild(newProject: Project, oldProject: Project): Boolean =
             newProject.build.buildArgs != oldProject.build.buildArgs ||
                     newProject.build.dockerFile != oldProject.build.dockerFile ||
-                    newProject.build.resources.memoryMb != oldProject.build.resources.memoryMb ||   // also this: if e.g. build memory increases from 1024 to 2048, we want a full rebuild.
+                    // also this: if e.g. build memory increases from 1024 to 2048, we want a full rebuild: the old project might have failed to build because there was not enough memory
+                    newProject.build.resources.memoryMb != oldProject.build.resources.memoryMb ||
                     newProject.gitRepo != oldProject.gitRepo
 
         private val json = Json { ignoreUnknownKeys = true; coerceInputValues = true }
