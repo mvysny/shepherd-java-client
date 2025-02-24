0c93e0ebafaa41e00ec17dc6686e4845a0456ade
Martin Vysny <mavi@vaadin.com>
2025-02-24 11:21:47 +0200

    ProjectListRouteTest: more tests

--- shepherd-web/src/test/kotlin/ui/ProjectListRouteTest.kt
+++ shepherd-web/src/test/kotlin/ui/ProjectListRouteTest.kt
@@ -1,8 +1,13 @@
 package com.github.mvysny.shepherd.web.ui
 
 import com.github.mvysny.kaributesting.v10._expect
+import com.github.mvysny.kaributesting.v10._get
+import com.github.mvysny.kaributesting.v10.expectRow
+import com.github.mvysny.kaributesting.v10.expectRows
 import com.github.mvysny.kaributools.navigateTo
+import com.github.mvysny.shepherd.api.ProjectView
 import com.github.mvysny.shepherd.web.AbstractAppTest
+import com.vaadin.flow.component.grid.Grid
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -14,5 +19,8 @@ class ProjectListRouteTest : AbstractAppTest() {
 
     @Test fun smoke() {
         _expect<ProjectListRoute>()
+        val grid = _get<Grid<ProjectView>>()
+        grid.expectRows(1)
+        grid.expectRow(0, "RouterLink[text='vaadin-boot-example-gradle']", "Martin Vysny <mavi@vaadin.com>", "Anchor[text='https://github.com/mvysny/vaadin-boot-example-gradle', href='https://github.com/mvysny/vaadin-boot-example-gradle']", "PublishedURLsAsVerticalLayout[@style='width:100%']", "BuildLinks[]", "Button[icon='vaadin:trash', @theme='small tertiary-inline icon']")
     }
 }
